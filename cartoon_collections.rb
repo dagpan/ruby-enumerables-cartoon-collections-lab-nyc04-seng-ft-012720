22af4bda8f2d884f1b05defc85cda052d31897a1
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-22 01:32:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,9 @@
 def roll_call_dwarves(source_array)
   # code an argument here
   # Your code here
+  
   i = 0
-  pp source_array
+
   source_array.length.times do
     puts "#{source_array[i]}/n"
     i += 1
